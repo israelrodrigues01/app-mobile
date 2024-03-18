40b91ce2b96223ff57cfe8a66d84a1b5fad860a4
Lucassec1 <lucasemmanuelcs@gmail.com>
2024-03-18 16:43:45 -0300

    feat: adiciono tela de vizualizar tarefa

--- src/app/task/get/index.tsx
+++ src/app/task/get/index.tsx
@@ -1,32 +1,32 @@
 import { View, Text } from "react-native";
 import { styles } from '../style';
+
 import Title from '../../../components/title/index';
+import StateButton from "../../../components/stateButton";
 
 export default function Task() {
   return (
     <View className="flex-1" style={styles.container} >
       <Title title="Tarefa" />
 
-      <View style={styles.card}>
-        <View style={styles.cardHeader}>
+      <View>
+        <View style={styles.header}>
           <Text style={styles.title}>Projeto Mobile</Text>
         </View>
-        <View className="px-4 py-6">
-          <View style={styles.cardContent}>
-            <Text>Descrição</Text>
-            {/* <Tag label="Alta"/> */}
+        <View className="px-4 py-6" style={styles.content}>
+          <View style={styles.info}>
+            <Text className="font-medium">Descrição</Text>
+            <StateButton text="Alta" color="#FB6565"/>
           </View>
-          <Text className="mt-6 font-medium">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor.</Text>
-          <View className="mt-2">
-            <Text className="font-bold">Status</Text>
-            <View className="bg-cyan-600 mt-1" style={{ borderWidth: 2, borderColor: "red"}}>
-              <Text>Em andamento</Text>
+          <Text className="mt-5 font-medium">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor.</Text>
+          <View className="mt-3">
+            <View className="mt-2">
+              <Text className="font-medium mb-1">Status</Text>
+              <StateButton text="Em andamento" color="#5595E0"/>
             </View>
-          </View>
-          <View className="mt-2">
-            <Text className="font-bold">Tags</Text>
-            <View className="bg-cyan-600 mt-1" style={{ borderWidth: 2, borderColor: "red"}}>
-              <Text>Projeto</Text>
+            <View className="mt-2">
+              <Text className="font-medium mb-1">Tags</Text>
+              <StateButton text="Projeto" color="#5595E0"/>
             </View>
           </View>
         </View>
--- src/app/task/style.ts
+++ src/app/task/style.ts
@@ -7,28 +7,34 @@ export const styles = StyleSheet.create({
     padding: 50,
     paddingHorizontal: 20,
   },
-  card: {
-    display: "flex",
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "red",
-  },
   title: {
     textAlign: "center",
     color: colors.white,
     paddingVertical: 13
   },
-  cardHeader: {
+  header: {
     backgroundColor: colors.blue[200],
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
   },
-  cardContent: {
+  content: {
+    backgroundColor: colors.white,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+
+    elevation: 1,
+  },
+  info: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    gap: 10,
-    borderWidth: 2,
-    borderColor: "red",
-  }
+    alignItems: "center",
+  },
 });
